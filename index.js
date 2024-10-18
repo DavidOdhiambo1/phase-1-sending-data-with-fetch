0f919ee3c4038f392a6467193d703eeb8511ca48
DavidOdhiambo1 <davidworkmail2021@gmail.com>
2024-10-18 15:01:21 +0300

    solution

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Add your code here
+function submitData (name, email){
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name,
+            email
+        })
+    })
+    .then(res => res.json())
+    .then(users => {
+        const id = document.createElement('h2')
+        id.textContent = users.id
+        console.log(id)
+        const body =document.querySelector('body') 
+        body.appendChild(id)  
+    })
+    .catch(error => {
+        alert("Something went wrong!!")
+        const errorMessage = document.createElement('p')
+        errorMessage.textContent = error.message
+        const body =document.querySelector('body')
+        body.appendChild(errorMessage)
+        
+    })
+
+}
+
+
+
+
